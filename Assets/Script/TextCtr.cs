83b00f48fbf33b8b4c9f920aa0362b0daa34d627
zhouy546 <zhouyunqi546@gmail.com>
2018-06-22 17:35:51 +0800

    commit

--- Assets/Script/TextCtr.cs
+++ Assets/Script/TextCtr.cs
@@ -19,10 +19,13 @@ public class TextCtr : MonoBehaviour {
             if (sToggle)
             {
                 textBase.HideAll();
+                textBase.SetLocation(textBase.currentLocalPosition + Vector3.up * 20, 1f, LeanTweenType.easeInOutQuad);
+
             }
             else
             {
                 textBase.ShowAll();
+                textBase.SetLocation(textBase.startLocalPosition, 1f, LeanTweenType.easeInQuad);
                 textBase.ChangeColor(Color.green, 1f, LeanTweenType.easeInCirc);
 
             }
