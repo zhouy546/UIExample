0be9952d5c7e1e03a6c8e7a04b8a842dbadf7391
zhouy546 <zhouyunqi546@gmail.com>
2018-06-22 14:34:00 +0800

    Create textbase

---
+++ Assets/Script/TextBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent (typeof (Text))]
+public class TextBase : Rect {
+
+	// Use this for initialization
+	public void Start () {
+		
+	}
+	
+}
